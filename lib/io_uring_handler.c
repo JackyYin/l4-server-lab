189e2f3d86f0d8bb245d956170dce854c998fb8e
JackyYin <jjyyg1123@gmail.com>
2022-03-18 11:48:57 +0800

    fix: remove io_uring_user_data
    sqe.user_data is only 64 bits long, so directly store a pointer to
    struct server_connection and use connection.action to store
    IO_URING_OP_X operation.

--- lib/io_uring_handler.c
+++ lib/io_uring_handler.c
@@ -10,37 +10,28 @@
 #define IO_URING_OP_READ (2)
 #define IO_URING_OP_WRITE (3)
 
-// this structure can't be larger than 64 bytes
-struct io_uring_user_data {
-    void *ptr; // point to connection
-    int op;
-};
-
-static void io_uring_push_accept(int fd, struct io_uring *ring)
+static void io_uring_push_accept(int fd, struct server_connection *conn,
+                                 struct io_uring *ring)
 {
     struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
     io_uring_prep_accept(sqe, fd, NULL, NULL, 0);
 
-    struct io_uring_user_data *ud =
-        (struct io_uring_user_data *)&sqe->user_data;
-    ud->op = IO_URING_OP_ACCEPT:
+    conn->action = IO_URING_OP_ACCEPT;
+    sqe->user_data = (uint64_t)conn;
 }
 
-static void io_uring_push_read(int fd, void *buf, size_t buflen, void *conn,
+static void io_uring_push_read(int fd, void *buf, size_t buflen,
+                               struct server_connection *conn,
                                struct io_uring *ring)
 {
     struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
     io_uring_prep_recv(sqe, fd, buf, buflen, 0);
 
-    struct io_uring_user_data *ud =
-        (struct io_uring_user_data *)&sqe->user_data;
-    ud->op = IO_URING_OP_READ;
-    ud->ptr = conn;
+    conn->action = IO_URING_OP_READ;
+    sqe->user_data = (uint64_t)conn;
 }
 
-static void io_uring_co_echo_handler(coroutine *co, void *data)
-{
-}
+static void io_uring_co_echo_handler(coroutine *co, void *data) {}
 
 void io_uring_listen_loop(pthread_t tid, struct server_info *svr)
 {
@@ -59,7 +50,9 @@ void io_uring_listen_loop(pthread_t tid, struct server_info *svr)
         LOG_ERROR("IORING_FAST_POLL is not available");
         return;
     }
-    io_uring_push_accept(svr->listen_fd, &ring);
+
+    struct server_connection *listen_conn = &svr->conns[svr->listen_fd];
+    io_uring_push_accept(svr->listen_fd, listen_conn, &ring);
 
     while (1) {
         io_uring_submit_and_wait(&ring, 1);
@@ -70,39 +63,43 @@ void io_uring_listen_loop(pthread_t tid, struct server_info *svr)
         {
             count++;
             ret = cqe->res;
-            struct io_uring_user_data *ud =
-                (struct io_uring_user_data *)&cqe->user_data;
-            if (ud->op == IO_URING_OP_ACCEPT) {
+            struct server_connection *conn =
+                (struct server_connection *)cqe->user_data;
+            if (UNLIKELY(!conn)) {
+                LOG_ERROR("Something went wrong, no user_data in cqe!\n");
+                continue;
+            }
+
+            if (conn->action == IO_URING_OP_ACCEPT) {
                 // check accept4 return
                 if (UNLIKELY(ret < 0)) {
-                    if (ret != -EAGAIN) {
-                        LOG_ERROR(
-                            "[%lu] Something went wrong with accept: %d\n", tid,
-                            ret);
-                        exit(1);
-                    }
+                    if (ret == -EAGAIN)
+                        goto ACCEPT_AGAIN;
+
+                    LOG_ERROR("[%lu] Something went wrong with accept: %d\n",
+                              tid, ret);
+                    exit(1);
                 } else {
                     LOG_INFO("socket accepted...\n");
                 }
 
-                struct server_connection *conn = &svr->conns[ret];
-                if (UNLIKELY(__prepare_connection(tid, ret, conn,
-                                                  io_uring_co_echo_handler) < 0)) {
+                struct server_connection *acc_conn = &svr->conns[ret];
+                if (UNLIKELY(__prepare_connection(tid, ret, acc_conn,
+                                                  io_uring_co_echo_handler) <
+                             0)) {
+                    LOG_ERROR("Failed to create connection...\n");
                     __close(ret);
                     goto ACCEPT_AGAIN;
                 }
 
                 // push new fd to wait for read
-                io_uring_push_read(ret, conn->buf.buf, conn->buf.capacity,
-                                   (void *)conn, &ring);
+                io_uring_push_read(ret, acc_conn->buf.buf,
+                                   acc_conn->buf.capacity, (void *)acc_conn,
+                                   &ring);
             ACCEPT_AGAIN:
-                io_uring_push_accept(svr->listen_fd, &ring);
-            } else if (ud->ptr) {
-                struct server_connection *conn =
-                    (struct server_connection *)ud->ptr;
-                int64_t yielded = co_resume(conn->coro);
+                io_uring_push_accept(svr->listen_fd, listen_conn, &ring);
             } else {
-                LOG_ERROR("Something went wrong, no user_data in cqe!\n");
+                /* co_resume(conn->coro); */
             }
         }
         /* io_uring_cqe_seen(&ring, cqe); */
