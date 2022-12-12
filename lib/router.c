9eedae9f0b6437f986498c8f509376f3308b9e46
7FrogTW <jjyyg1123@gmail.com>
2022-12-12 16:33:47 +0800

    fix: read custom section data from Elf_Data of sec

--- lib/router.c
+++ lib/router.c
@@ -10,55 +10,11 @@ static int get_self_fd()
     return __open("/proc/self/exe", O_RDONLY);
 }
 
-static Gelf_Sym * load_symbols(Elf_Data *sym_data, size_t *symbol_cnt)
-{
-    if (!sym_data || !symbol_cnt)
-        return NULL;
-
-    Gelf_Sym *sym = malloc(sym_data->d_size);
-    if (sym) {
-        for (int i = 0; i < sym_data->d_size/sizeof(Gelf_Sym); i++) {
-            gelf_getsym(sym_data, i, &sym[i]);
-        }
-        *symbol_cnt = sym_data->d_size/sizeof(Gelf_Sym);
-    }
-    return sym;
-}
-
-static void get_router_symbols(Elf *elf, GElf_Ehdr *ehdr, int routerndx, GElf_Sym *symbols, size_t symbol_cnt)
-{
-    GElf_Shdr shdr;
-    Elf_Scn *scn = elf_getscn(elf, routerndx);
-    if (!scn)
-        goto err;
-
-    Elf_Data *data = elf_getdata(scn, NULL);
-    if (!data)
-        goto err;
-
-    for (size_t i = 0; i < symbol_cnt; i++) {
-        GElf_Sym symbol = symbols[i];
-
-        if (symbol.st_shndx != routerndx)
-            continue;
-
-        char *symname = elf_strptr(elf, ehdr.shstrndx, symbol.st_name);
-        LOG_INFO("symbol name: %s, symbol info: %d, symbol value: %lu, symbol size: %lu\n",symname, symbol.st_info, symbol.st_value, symbol.st_size);
-
-        struct router *r = (struct router *)(data->d_buf + symbol.st_value);
-        LOG_INFO("router path: %s, filepath: %s, method: %d\n", r->path, r->filepath, r->method);
-    }
-err:
-}
-
 static void load_router_via_elf()
 {
     Elf *elf;
     GElf_Ehdr ehdr;
     GElf_Shdr shdr;
-    GElf_Sym *symbols;
-    int routerndx = -1;
-    size_t symbol_cnt;
 
     int fd = get_self_fd();
     if (fd < 0)
@@ -74,25 +30,28 @@ static void load_router_via_elf()
 
     for (size_t i = 0; i < ehdr.e_shnum; i++) {
         Elf_Scn *scn = elf_getscn(elf, i);
+        Elf_Data *data = NULL;
         if (!scn)
             continue;
 
         if (!gelf_getshdr(scn, &shdr))
             continue;
 
-        if (shdr.sh_type == SHT_SYMTAB) {
-            Elf_Data *data = elf_getdata(scn, NULL);
-            symbols = load_symbols(data, symbol_cnt);
-        } else {
-            char *secname = elf_strptr(elf, ehdr.shstrndx, shdr.sh_name);
-            if (secname && !strncmp(secname, "ROUTER", sizeof("ROUTER")))
-                routerndx = i;
+        char *secname = elf_strptr(elf, ehdr.shstrndx, shdr.sh_name);
+        if (secname && !strncmp(secname, "ROUTER", sizeof("ROUTER"))) {
+            while ((data = elf_getdata(scn, data))) {
+                if (!data->d_buf)
+                    continue;
+
+                for (int j = 0; j < data->d_size / sizeof(struct router); j++) {
+                    struct router *base = (struct router *)data->d_buf + j;
+                    fprintf(stdout, "base: %p\n", base);
+                    fprintf(stdout, "path: %s, filepath: %s, method: %d\n", base->path,  base->filepath, base->method);
+                }
+            }
         }
     }
 
-    if (routerndx >= 0 && symbols) {
-        get_router_symbols(elf, &ehdr, routerndx, symbols, symbol_cnt);
-    }
 close:
     __close(fd);
 err:
